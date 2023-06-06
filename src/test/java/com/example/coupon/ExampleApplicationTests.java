a036db25a675e0ee22b266b0a675e63583e8d6f5
yujung7768903 <yu-jung31476@naver.com>
2023-06-06 16:21:30 +0900

    test: 예외를 잡지 않고 던지도록 수정 및 통합 테스트 추가
    - 추가한 통합 테스트: 코드 중복 에러 발생 테스트, 1000개 쿠폰 등록 소요 시간 체크

--- src/test/java/com/example/coupon/ExampleApplicationTests.java
+++ src/test/java/com/example/coupon/ExampleApplicationTests.java
@@ -1,13 +1,83 @@
 package com.example.coupon;
 
+import com.example.coupon.domain.CouponScope;
+import com.example.coupon.dto.CouponSaveRequestDto;
+import com.example.coupon.service.CouponService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.io.FileInputStream;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@Slf4j
 @SpringBootTest
 class ExampleApplicationTests {
 
+	@Autowired
+	private CouponService couponService;
+
 	@Test
-	void contextLoads() {
+	@Transactional
+	@DisplayName("데이터베이스에 중복된 코드 존재시 에러 발생")
+	void duplicateCodeInDatabase() throws Exception {
+		// given
+		String fileName = "데이터베이스내_코드중복체크용.csv";
+		String path = "src/test/resources/" + fileName;
+		DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+
+		MockMultipartFile multipartFile = new MockMultipartFile(fileName, new FileInputStream(path));
+		CouponSaveRequestDto couponSaveRequestDto = CouponSaveRequestDto.builder()
+				.csvFile(multipartFile)
+				.discount(50)
+				.scope(CouponScope.ALL)
+				.expirationStartDate(LocalDateTime.parse("2023-05-27T00:00:00", dateTimeFormatter))
+				.expirationEndDate(LocalDateTime.parse("2023-06-25T00:00:00", dateTimeFormatter))
+				.description("전품목 50% 할인 쿠폰")
+				.build();
+
+		// when
+		couponService.registerCoupon(couponSaveRequestDto);
+
+		// then
+		assertThrows(IllegalArgumentException.class, () -> couponService.registerCoupon(couponSaveRequestDto));
+	}
+
+	@Test
+	@Transactional
+	@DisplayName("1000개의 쿠폰 등록시 소요 시간 체크")
+	void checkRegisterCouponTime1000() throws Exception {
+		// given
+		String fileName = "일괄등록_쿠폰_1000개.csv";
+		String path = "src/test/resources/" + fileName;
+		DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+
+		MockMultipartFile multipartFile = new MockMultipartFile(fileName, new FileInputStream(path));
+		Instant start = Instant.now();
+		CouponSaveRequestDto couponSaveRequestDto = CouponSaveRequestDto.builder()
+				.csvFile(multipartFile)
+				.discount(50)
+				.scope(CouponScope.ALL)
+				.expirationStartDate(LocalDateTime.parse("2023-05-27T00:00:00", dateTimeFormatter))
+				.expirationEndDate(LocalDateTime.parse("2023-06-25T00:00:00", dateTimeFormatter))
+				.description("전품목 50% 할인 쿠폰")
+				.build();
+
+		// when
+		couponService.registerCoupon(couponSaveRequestDto);
+
+		// then
+		Instant end = Instant.now();
+		log.info("1000개 쿠폰 등록 소요 시간: {}ms", Duration.between(start, end).toMillis());
 	}
 
 }
--- src/test/java/com/example/coupon/controller/CouponControllerTest.java
+++ src/test/java/com/example/coupon/controller/CouponControllerTest.java
@@ -1,5 +1,6 @@
 package com.example.coupon.controller;
 
+import com.example.coupon.dto.CouponSaveRequestDto;
 import com.example.coupon.service.CouponService;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
@@ -10,11 +11,10 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.ArrayList;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -34,36 +34,30 @@ public class CouponControllerTest {
     private CouponService couponService;
 
     @Test
-    public void registerCouponTest() {
+    public void registerCouponTest() throws Exception {
         // given
-        given(couponService.registerCoupon(any())).willReturn(new ArrayList<>());
+        given(couponService.registerCoupon(any(CouponSaveRequestDto.class))).willReturn(new ArrayList<>());
         String fileName = "일괄등록_쿠폰_1000개.csv";
         String path = "src/test/resources/" + fileName;
+        MockMultipartFile multipartFile = new MockMultipartFile(fileName, new FileInputStream(path));
 
-        try {
-            MockMultipartFile multipartFile = new MockMultipartFile(fileName, new FileInputStream(path));
+        // when
+        ResultActions resultActions = mockMvc.perform(multipart("/coupon")
+                .file(multipartFile)
+                .param("discount", "50")
+                .param("scope", "ALL")
+                .param("description", "전품목 50% 할인 쿠폰")
+                .param("expirationStartDate", "2023-05-27T00:00:00")
+                .param("expirationEndDate", "2023-06-27T00:00:00")
+        );
 
-            // when
-            ResultActions resultActions = mockMvc.perform(multipart("/coupon")
-                    .file(multipartFile)
-                    .param("discount", "50")
-                    .param("scope", "ALL")
-                    .param("description", "전품목 50% 할인 쿠폰")
-                    .param("expirationStartDate", "2023-05-27T00:00:00")
-                    .param("expirationEndDate", "2023-06-27T00:00:00")
-            );
 
-            // then
-            resultActions.andExpect(status().isCreated());
-        } catch (FileNotFoundException e) {
-            log.info("파일을 찾을 수 없습니다.");
-        } catch (IOException e) {
-            log.info("MultipartFile 생성 중 문제가 발생했습니다.");
-        } catch (SecurityException e) {
-            log.info("파일에 대한 읽기 권한이 없습니다.");
-        } catch (Exception e) {
-            log.info("Controller 테스트 중 문제가 발생했습니다.");
-        }
+        // then
+        MvcResult mvcResult = resultActions
+                .andExpect(status().isCreated())
+                .andReturn();
+
+        log.info(mvcResult.getResponse().getContentAsString());
     }
 
 }
