6454f939c4b5fd19e19a5e64b7085e147d2d9c5a
yujung7768903 <yu-jung31476@naver.com>
2023-06-06 16:03:35 +0900

    feat: 쿠폰 데이터 개별 등록 로직 추가

--- src/main/java/com/example/coupon/service/CouponService.java
+++ src/main/java/com/example/coupon/service/CouponService.java
@@ -24,13 +24,13 @@ public class CouponService {
 
     @Transactional
     public List<String> registerCoupon(CouponSaveRequestDto requestDto) {
-        requestDto.getCsvFile();
         try(InputStreamReader inputStreamReader = new InputStreamReader(requestDto.getCsvFile().getInputStream())) {
             try (CSVReader csvReader = new CSVReader(inputStreamReader)) {
                 List<String> codeList = csvReader.readAll().stream()
                         .map(arr -> arr[0])
                         .collect(Collectors.toList());
                 checkDuplicateCoupon(codeList);
+                codeList.forEach(c -> couponRepository.save(requestDto.toCoupon(c)));
                 return codeList;
             } catch (CsvException e) {
                 throw new RuntimeException(e);
@@ -45,7 +45,7 @@ public class CouponService {
         if (codeList.size() != new HashSet<>(codeList).size()) {
             throw new IllegalArgumentException("csv 파일 내 중복된 쿠폰 코드가 있습니다.");
         } else if (couponRepository.countByCodeIn(codeList) > 0) {
-            throw new IllegalArgumentException("중복된 쿠폰 코드가 등록되어 있습니다..");
+            throw new IllegalArgumentException("중복된 쿠폰 코드가 등록되어 있습니다.");
         }
     }
 
