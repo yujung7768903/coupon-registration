22a101f0e9cbe0c153e9702f1742bd3a0503f27a
yujung7768903 <yu-jung31476@naver.com>
2023-06-17 23:11:29 +0900

    test: 일괄등록 테스트 및 시간 측정 로직 추가

--- src/test/java/com/example/coupon/ExampleApplicationTests.java
+++ src/test/java/com/example/coupon/ExampleApplicationTests.java
@@ -1,14 +1,17 @@
 package com.example.coupon;
 
 import com.example.coupon.domain.CouponScope;
+import com.example.coupon.dto.CouponDeleteRequestDto;
 import com.example.coupon.dto.CouponSaveRequestDto;
 import com.example.coupon.service.CouponService;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.FileInputStream;
@@ -21,10 +24,14 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @Slf4j
 @SpringBootTest
+@ActiveProfiles("dev")
 class ExampleApplicationTests {
 
 	@Autowired
 	private CouponService couponService;
+
+	@Value("${spring.datasource.url}")
+	private String url;
 
 	@Test
 	@Transactional
@@ -33,9 +40,9 @@ class ExampleApplicationTests {
 		// given
 		String fileName = "데이터베이스내_코드중복체크용.csv";
 		String path = "src/test/resources/" + fileName;
+		MockMultipartFile multipartFile = new MockMultipartFile(fileName, new FileInputStream(path));
 		DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
 
-		MockMultipartFile multipartFile = new MockMultipartFile(fileName, new FileInputStream(path));
 		CouponSaveRequestDto couponSaveRequestDto = CouponSaveRequestDto.builder()
 				.csvFile(multipartFile)
 				.discount(50)
@@ -54,15 +61,61 @@ class ExampleApplicationTests {
 
 	@Test
 	@Transactional
-	@DisplayName("1000개의 쿠폰 등록시 소요 시간 체크")
-	void checkRegisterCouponTime1000() throws Exception {
+	@DisplayName("단건 등록시 소요 시간 체크")
+	void checkRegisterCouponTime() throws Exception {
 		// given
-		String fileName = "일괄등록_쿠폰_1000개.csv";
+		String fileName = "일괄등록_쿠폰_10000개.csv";
 		String path = "src/test/resources/" + fileName;
-		DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
-
 		MockMultipartFile multipartFile = new MockMultipartFile(fileName, new FileInputStream(path));
+
+		long timeSum = 0;
+		long n = 5;
+
+		log.info("쿠폰 단건 등록 시작");
+		// when
+		for (int i = 0; i < n; i++) {
+			deleteCoupon(multipartFile);
+			long executionTime = registerCoupon(multipartFile, false);
+			timeSum += executionTime;
+			log.info("{}번째 쿠폰 등록 소요 시간: {}ms", i, executionTime);
+		}
+
+		// then
+		log.info("Database url: {}", url);
+		log.info("쿠폰 등록 파일 이름: {}", fileName);
+		log.info("단건 등록 평균 시간: {}ms", timeSum / n);
+	}
+
+	@Test
+	@DisplayName("일괄 등록시 소요 시간 체크")
+	void checkbulkRegisterCouponTime() throws Exception {
+		// given
+		String fileName = "일괄등록_쿠폰_10000개.csv";
+		String path = "src/test/resources/" + fileName;
+		MockMultipartFile multipartFile = new MockMultipartFile(fileName, new FileInputStream(path));
+
+		long timeSum = 0;
+		long n = 1;
+
+		log.info("쿠폰 일괄 등록 시작");
+		// when
+		for (int i = 0; i < n; i++) {
+			deleteCoupon(multipartFile);
+			long executionTime = registerCoupon(multipartFile, true);
+			timeSum += executionTime;
+			log.info("{}번째 쿠폰 등록 소요 시간: {}ms", i, executionTime);
+		}
+
+		// then
+		log.info("Database url: {}", url);
+		log.info("쿠폰 등록 파일 이름: {}", fileName);
+		log.info("일괄 등록 평균 시간(일괄 등록): {}ms", timeSum / 5);
+	}
+
+	long registerCoupon(MockMultipartFile multipartFile, boolean isBulkRegistration) {
+		DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
 		Instant start = Instant.now();
+
 		CouponSaveRequestDto couponSaveRequestDto = CouponSaveRequestDto.builder()
 				.csvFile(multipartFile)
 				.discount(50)
@@ -72,12 +125,20 @@ class ExampleApplicationTests {
 				.description("전품목 50% 할인 쿠폰")
 				.build();
 
-		// when
-		couponService.registerCoupon(couponSaveRequestDto);
+		if (isBulkRegistration) {
+			couponService.bulkRegisterCoupon(couponSaveRequestDto);
+		} else {
+			couponService.registerCoupon(couponSaveRequestDto);
+		}
 
-		// then
 		Instant end = Instant.now();
-		log.info("1000개 쿠폰 등록 소요 시간: {}ms", Duration.between(start, end).toMillis());
+		return Duration.between(start, end).toMillis();
+	}
+
+	void deleteCoupon(MockMultipartFile multipartFile) {
+		CouponDeleteRequestDto couponDeleteRequestDto = new CouponDeleteRequestDto(multipartFile);
+
+		couponService.deleteCoupon(couponDeleteRequestDto);
 	}
 
 }
